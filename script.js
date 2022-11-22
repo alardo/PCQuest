7b5dc94e24219889d9db5265ffac09c6e61f267b
alardo <emilianov.siciliani@gmail.com>
2022-11-22 20:35:18 +0100

    v0.2.7

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var codeprog1 =1;
 var speed = 2000;
-msgArr = ["Nice to meet you (again), Mr. Smith...","You have an 'Insert Coin' on the back of your head, right?","You unleashed the Kraken!","All belongs to me, from every bit to the motherboard!","Try to format yourself!","Even 8bit of my RAM can think as fast as you do, Mr. Smith!","You son of a bitchboard!","How did you get your computer science degree? Playing ATARI games?","Your GameBoy Advance is more Advanced than you!","Turn yourself on 'n' off, again. Maybe it can help..."];
+msgArr = ["Nice to meet you (again), Mr. Smith...","You have an 'Insert Coin' on the back of your head, right?","You unleashed the Kraken!","All belongs to me, from every bit to the motherboard!","Try to format yourself!","Even 8bit of my RAM can think as fast as you do, Mr. Smith!","You son of a BitchBoard!","How did you get your computer science degree? Playing ATARI games?","Your GameBoy Advance is more Advanced than you!","Turn yourself on 'n' off, again. Maybe it can help..."];
   $( function() {
+var crime = document.getElementById("crime");
+  crime.play();
 setInterval(changeMsg, 10000);
 var progressbar = $( "#prog" );
       // $('#logo').show();     
@@ -125,7 +127,9 @@ var msg = $("#msg");
   $("#k").click(function(){
     changeMsg();
     var flu = document.getElementById("flu");
-    flu.play();
+    flu.play
+var a = document.getElementById("ahah");
+    a.play();
   });
   
   $('#txt1, #txt2, #txt3, #img1, #img2').click(function(){
@@ -170,11 +174,14 @@ var msg = $("#msg");
       });
   });
   function changeMsg(){
+    
     var nr = Math.floor(Math.random()*10);
     $("#msg").html(msgArr[nr]);
     //$("#msg").show();
-    //var flu = document.getElementById("flu");
-    //flu.play();
+    if(codeprog1 > 6){
+    var ah = document.getElementById("ahah");
+    ah.play();
+    }
   }
   var i = 3;
   function serc1()
@@ -203,6 +210,10 @@ var msg = $("#msg");
   }else if(url=='http://www.uniteduni.edu/upload'){
     $('#dialogal').html("<h4>UnitedUNI.edu</h4><p>Paste here the local reference to the file you want to upload and hit enter</p>");
     $('#dialogal').dialog('open');
+  }else if(url== 'http://www.microsoft.com/becomeoneofus' && codeprog1 == 6){
+    $('#dialogal').html("<h4>Microsoft Windows: Join us!!!</h4><p>If you want to install our system in you machine, the first thing you need to do, is to format your Hard Drive, and backup you personal data. To do so, once you had saved all important file to another drive, launch the command:<br>format *name of the drive*;<br>for example<br>format C;</p>");
+    $('#dialogal').dialog('open');
+    codeprog1=7;
   }else if(url == '0://Files/mywork.exec' && codeprog1==2){
     codeprog1 = 3;
     $('#dialogal').html("<h4>UnitedUNI.edu</h4><p>File successfully uploaded</p>");
@@ -211,7 +222,7 @@ var msg = $("#msg");
     $('#end1').html('Some months later,<br> something happens...');
     $('#end1').toggle('fade',2000);
     $('#mail').accordion('destroy');
-    $('#mail').html('<h2>News: Tech.net</h2><p>Next Microsoft Windows realese is up for next month.</p><h2>You don\'t know me, but in a way you do...<br>[From: j.parker@uniteduniversity.edu]</h2><p>Hello, my friend, my name is John.<br> I\'ve seen what you had uploaded. You don\'t know me, but in a sense you do. I\'m the one who created destroyme.code, and who build mywork.exec to create that. I\'ve been working hard, all to destroy K. Klopp and his virus, Cli.exec. All came from this OS, Zer0. Cli was there from the beginning. Maybe was mean to be there. And there to stay... But to erease him, you need to face him, in front of your eyes. You need to stand the truth: you must see to know, what you are going to defeat.<br>Best regards, my friend. Where I fall could you stand tall.</p>');
+    $('#mail').html('<h2>News: Tech.net</h2><p>Next Microsoft Windows realese is up for next month. The Team is working hard to make this thing happen. If you wish to switch from your old system to this, visit the Official Microsoft Windows page: http://www.microsoft.com/becomeoneofus</p><h2>You don\'t know me, but in a way you do...<br>[From: j.parker@uniteduniversity.edu]</h2><p>Hello, my friend, my name is John.<br> I\'ve seen what you had uploaded. You don\'t know me, but in a sense you do. I\'m the one who created destroyme.code, and who build mywork.exec to create that. I\'ve been working hard, all to destroy K. Klopp and his virus, Cli.exec. All came from this OS, Zer0. Cli was there from the beginning. Maybe was mean to be there. And there to stay... But to erease him, you need to face him, in front of your eyes. You need to stand the truth: you must see to know, what you are going to defeat.<br>Best regards, my friend. Where I fall could you stand tall.<br>(Beawere, Cli.exec is more wise than you think. And this Zer0 OS system, is becoming too small for his Ego...)</p>');
     $('#mail').accordion({
       heightStyle: "auto"
     });
@@ -242,6 +253,8 @@ var msg = $("#msg");
         $("#msg").show();
         var audiok = document.getElementById("audiok");
         audiok.play();
+      var ah = document.getElementById("ahah");
+      ah.play();
         //$('#dialogal').html('Hello World!!!<br>I\'m finally alive!');
         //$('#dialogal').dialog('open');
       }else if(url=='0://Files/destroyme.code' && codeprog1 == 4){
@@ -249,10 +262,19 @@ var msg = $("#msg");
         $("#mecode").show();
         $('#dialogal').html('File Executed.');
         $('#dialogal').dialog('open');
+      }else if(url=='0://Files/destroyme.code' && codeprog1 == 8){
+
+      $('#end').html('End N.2:<br>Dear, Mr. Smith:<br>All we can do (maybe not enough but) was done to stop Clippy. In the future our story will be not remebered. But Clippy will be not, too. Have an happy life, away, do it for me, from the keyboard...<br>Your, John.');
+     $('#end').toggle('fade',2000);
+      $('#prog').toggle('fade',3000);
+      $('#prog').progressbar('value', 0);
+      $('#prog').addClass('progressbar');
+      $('#fore').toggle('fade',3000);
+        
       }else if(url=='0://Files/destroyme.code' && codeprog1 == 2){
      // $('#logo').toggle('fade', 5000);
      //$('#dialog').dialog('close');
-     $('#end').html('Good Luck, Mr. Smith:<br>The virus it\'s gone forever.<br>Are you brave enough, to tell us it\'s been a good choice?');
+     $('#end').html('End N.1:<br>Good Luck, Mr. Smith:<br>The virus it\'s gone forever.<br>Are you brave enough, to tell us it\'s been a good choice?');
      $('#end').toggle('fade',2000);
       $('#prog').toggle('fade',3000);
       $('#prog').progressbar('value', 0);
@@ -294,7 +316,14 @@ setTimeout(prog3, 100);
   function op(){
     var url = $("#toOp").val();
     var func = "//execute to destroy me\ndefine pathToFile = '0://Files/';\ndefine fileName = 'cli.exec';\ndefine isHidden = true;\ndefine destroyIt = true;\ndefine path = pathToFile+fileName;\n\t return [path, isHidden, destroyIt];";
-    if(url == "0://Files/destroyme.code"){
+    var func2 = "//execute to destroy me\ndefine pathToFile = '0://Files/';\ndefine fileName = 'cli.exec';\ndefine isHidden = false;\ndefine destroyIt = false;\ndefine path = pathToFile+fileName;\n\t return [path, isHidden, destroyIt];";
+    if(url == "0://Files/destroyme.code" && codeprog1 == 7){
+      $("#toSv").val(url);
+      $("#texta").val(func2);
+      $("#dialogal").html("File "+url+" opened!");
+      $("#dialogal").dialog("open");
+      
+    }else if(url == "0://Files/destroyme.code" && codeprog1 == 3){
       $("#toSv").val(url);
       $("#texta").val(func);
       $("#dialogal").html("File "+url+" opened!");
@@ -312,20 +341,26 @@ setTimeout(prog3, 100);
   
  function sv(){
    var url = $("#toOp").val();
-   var texta = $("#texta").val().replaceAll("define", "var");
+   var texta = $("#texta").val().replaceAll("define", "var").replaceAll('format','return');
    
    
   var fu = new Function(texta)();
   var ret = ['0://Files/cli.exec', false, false];
+  var ret2 = 0;
    //console.log(fu);
    //console.log(ret);
-   if(url == "0://Files/destroyme.code" && fu.toString() == ret.toString()){
-     $("#dialogal").html("Saved!");
+   if(url == "0://Files/destroyme.code" && fu.toString() == ret.toString() &&codeprog1==3){
+     $("#dialogal").html("Saved, No Errors!");
      $("#dialogal").dialog("open");
      codeprog1 = 4;
-   }else if(url == "0://Files/destroyme.code" && fu != ret){
+   }else if(url == "0://Files/destroyme.code" && fu != ret && codeprog1 == 3){
      $("#dialogal").html("Errors in the code!");
     $("#dialogal").dialog("open");
+   }else if(url == "0://Files/destroyme.code" && fu.toString() == ret2.toString() && codeprog1 == 7){
+     $("#dialogal").html("Saved, No Errors!");
+     $("#dialogal").dialog("open");
+     codeprog1 = 8;
+     
    }else{
    $("#dialogal").html("I/O Error: File is corrupted or don't exist!");
     $("#dialogal").dialog("open");   
